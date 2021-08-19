fc19fdf2467b99625c30ccd54ad5b49e2c6651a6
Parker Rueve <parkerrueve@gmail.com>
2021-08-19 13:35:54 -0400

    Handle phx-target attribute

--- src/phoenix-custom-event-hook.js
+++ src/phoenix-custom-event-hook.js
@@ -1,27 +1,43 @@
 const PhxCustomEvent = {
   mounted() {
     const attrs = this.el.attributes;
+    const phxTarget = attrs["phx-target"].value;
+    const pushEvent = phxTarget
+      ? (event, payload, callback) =>
+          this.pushEventTo(phxTarget, event, payload, callback)
+      : this.pushEvent;
+
     for (var i = 0; i < attrs.length; i++) {
       if (/^phx-custom-event-/.test(attrs[i].name)) {
         const eventName = attrs[i].name.replace("phx-custom-event-", "");
-        const phxEvent = attrs[i].value
+        const phxEvent = attrs[i].value;
         this.el.addEventListener(eventName, ({ detail }) => {
-          this.el.dispatchEvent(new CustomEvent('phx-event-start', { detail: { name: eventName, payload: detail } }));
-          this.pushEvent(phxEvent, detail, e => {
-            this.el.dispatchEvent(new CustomEvent('phx-event-complete', { detail: { name: eventName, payload: detail } }));
+          this.el.dispatchEvent(
+            new CustomEvent("phx-event-start", {
+              detail: { name: eventName, payload: detail },
+            })
+          );
+          pushEvent(phxEvent, detail, (e) => {
+            this.el.dispatchEvent(
+              new CustomEvent("phx-event-complete", {
+                detail: { name: eventName, payload: detail },
+              })
+            );
           });
         });
       }
     }
     if (this.el.getAttribute("phx-receive-events")) {
-      const phoenixEvents = this.el.getAttribute("phx-receive-events").split(",");
-      phoenixEvents.forEach(evt => {
+      const phoenixEvents = this.el
+        .getAttribute("phx-receive-events")
+        .split(",");
+      phoenixEvents.forEach((evt) => {
         this.handleEvent(evt, (payload) => {
-          this.el.dispatchEvent(new CustomEvent(evt, {detail: payload}));
+          this.el.dispatchEvent(new CustomEvent(evt, { detail: payload }));
         });
       });
     }
-  }
-}
+  },
+};
 
-export default PhxCustomEvent;  
+export default PhxCustomEvent;
