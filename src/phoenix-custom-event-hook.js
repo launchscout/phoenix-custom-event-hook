fdb2ff1c87d0e0a5d18ab65d8535f8a001a71df6
Chris Nelson <superchrisnelson@gmail.com>
2023-03-04 19:12:34 -0500

    checkin missing version

--- src/phoenix-custom-event-hook.js
+++ src/phoenix-custom-event-hook.js
@@ -1,31 +1,31 @@
 const PhxCustomEvent = {
   mounted() {
-    const attrs = this.el.attributes;
-    const phxTarget = attrs["phx-target"].value;
-    const pushEvent = phxTarget
-      ? (event, payload, callback) =>
+    const sendEvent = (eventName, phxEvent) => {
+      const attrs = this.el.attributes;
+      const phxTarget = attrs["phx-target"] && attrs["phx-target"].value;
+      const pushEvent = phxTarget
+        ? (event, payload, callback) =>
           this.pushEventTo(phxTarget, event, payload, callback)
-      : this.pushEvent;
+        : (event, payload, callback) => this.pushEvent(event, payload, callback);
+      this.el.addEventListener(eventName, ({ detail }) => {
+        this.el.dispatchEvent(new CustomEvent('phx-event-start', { detail: { name: eventName, payload: detail } }));
+        pushEvent(phxEvent, detail, e => {
+          this.el.dispatchEvent(new CustomEvent('phx-event-complete', { detail: { name: eventName, payload: detail } }));
+        });
+      });
+    };
 
+    const attrs = this.el.attributes;
     for (var i = 0; i < attrs.length; i++) {
       if (/^phx-custom-event-/.test(attrs[i].name)) {
         const eventName = attrs[i].name.replace("phx-custom-event-", "");
         const phxEvent = attrs[i].value;
-        this.el.addEventListener(eventName, ({ detail }) => {
-          this.el.dispatchEvent(
-            new CustomEvent("phx-event-start", {
-              detail: { name: eventName, payload: detail },
-            })
-          );
-          pushEvent(phxEvent, detail, (e) => {
-            this.el.dispatchEvent(
-              new CustomEvent("phx-event-complete", {
-                detail: { name: eventName, payload: detail },
-              })
-            );
-          });
-        });
+        sendEvent(eventName, phxEvent);
       }
+    }
+    if (this.el.getAttribute("phx-send-events")) {
+      const eventsToSend = this.el.getAttribute("phx-send-events").split(",");
+      eventsToSend.forEach((eventName) => sendEvent(eventName, eventName));
     }
     if (this.el.getAttribute("phx-receive-events")) {
       const phoenixEvents = this.el
